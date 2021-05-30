3b73d588e76f51f947cd0db8b8ca6ae6257c5540
You Song Hou <yousonghou@yahoo.com>
2021-05-29 22:53:53 -0400

    add a function to add coder's work to the contract

--- contracts/CodeworkNFT.sol
+++ contracts/CodeworkNFT.sol
@@ -2,11 +2,29 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CodeworkNFT is ERC721 {
+  mapping(uint => CodeWork) public codeworkList;
+
+  struct CodeWork {
+    uint tokenId;
+    string codeURL;
+    uint date;
+    uint price;
+    address payable from;
+  }
+
+  event CodeWorkSubmit (
+    uint tokenId,
+    string codeURLs,
+    uint date,
+    uint price,
+    address payable from
+  );
+
   event CodeworkCreated (
-      uint tokenId,
-      string metadataURL,
-      uint date,
-      address payable from
+    uint tokenId,
+    string metadataURL,
+    uint date,
+    address payable from
   );
 
   constructor() ERC721("Codework NFT", "CWN")  public {}
@@ -17,5 +35,10 @@ contract CodeworkNFT is ERC721 {
     _setTokenURI(_tokenId, _metadataURL);
 
     emit CodeworkCreated(_tokenId, _metadataURL, now, msg.sender);
+  }
+
+  function addCodeToWork(uint _tokenId, uint _price, string memory _codeURL) external {
+    codeworkList[_tokenId] = CodeWork(_tokenId, _codeURL, now, _price, msg.sender);
+    emit CodeWorkSubmit(_tokenId, _codeURL, now, _price, msg.sender);
   }
 }
