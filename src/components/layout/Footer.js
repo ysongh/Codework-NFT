d4dc3f30711e3615aee2eb7050145bfe51b4cb3d
You Song Hou <yousonghou@gmail.com>
2021-08-06 22:58:39 -0400

    add the links to project's email and twitter

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Icon } from 'semantic-ui-react';
 
 function Footer() {
   return (
-    <Segment inverted color='violet' style={{ textAlign: 'center', padding: '1.5rem 0'}}>
-      Copyright &copy; 2021 Codework NFT
+    <Segment inverted color='violet' style={{ textAlign: 'center', padding: '1rem 0'}}>
+      <a href="mailto:codeworknft@gmail.com" aria-label="Icon" target="_blank">
+        <Icon link size='big' name='mail' color='teal' style={{ marginBottom: '.5rem', marginRight: '1rem' }} />
+      </a>
+      <a href='https://twitter.com/codeworknft' aria-label="Icon" target='_blank' rel='noopener noreferrer'>
+        <Icon link size='big' name='twitter' color='teal' style={{ marginBottom: '.5rem' }} />
+      </a>
+      <p style={{ fontSize: '1.3rem' }}>Copyright &copy; 2021 Codework NFT</p>
     </Segment>
   )
 }
