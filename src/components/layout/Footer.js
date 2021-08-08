ba07692d075d051ad4113a429e204ec8d8e0d043
You Song Hou <yousonghou@gmail.com>
2021-08-07 23:09:27 -0400

    update the dapp's metadata

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import { Segment, Icon } from 'semantic-ui-react';
 function Footer() {
   return (
     <Segment inverted color='violet' style={{ textAlign: 'center', padding: '1rem 0'}}>
-      <a href="mailto:codeworknft@gmail.com" aria-label="Icon" target="_blank">
+      <a href='mailto:codeworknft@gmail.com' aria-label='Icon' target='_blank' rel='noopener noreferrer'>
         <Icon link size='big' name='mail' color='teal' style={{ marginBottom: '.5rem', marginRight: '1rem' }} />
       </a>
       <a href='https://twitter.com/codeworknft' aria-label="Icon" target='_blank' rel='noopener noreferrer'>
