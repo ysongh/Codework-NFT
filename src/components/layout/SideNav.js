83f8c89d83a251c9591a14ba25e74fa13acc062a
You Song Hou <yousonghou@gmail.com>
2021-12-02 00:53:08 -0500

    add icons on the sidebar

--- src/components/layout/SideNav.js
+++ src/components/layout/SideNav.js
@@ -14,38 +14,47 @@ function SideNav({ loadBlockchain, walletAddress, setAccount, setCodeworkNFTBloc
         <Icon name='home' />
         Home
       </Menu.Item>
+
       <Menu.Item
         as={Link}
         to="/addbounty"
-        name='Create Bounty'
         active={activeItem === 'Create Bounty'}
-        onClick={() => setActiveItem('Create Bounty')}
-      />
+        onClick={() => setActiveItem('Create Bounty')}>
+          <Icon name='bug' />
+          Create Bounty
+      </Menu.Item>
+
       <Menu.Item
         as={Link}
         to="/codenftlist"
-        name='Code NFT List'
         active={activeItem === 'Code NFT List'}
-        onClick={() => setActiveItem('Code NFT List')}
-      />
+        onClick={() => setActiveItem('Code NFT List')}>
+          <Icon name='grid layout' />
+          Code NFT List
+      </Menu.Item>
+
       <Menu.Item
         as={Link}
         to="/addcode"
-        name='Add Code'
         active={activeItem === 'Add Code'}
-        onClick={() => setActiveItem('Add Code')}
-      />
+        onClick={() => setActiveItem('Add Code')}>
+          <Icon name='code' />
+          Add Code
+      </Menu.Item>
+
       <Menu.Item
         as="a"
         href="https://docs.google.com/forms/d/e/1FAIpQLSekANSgKrBphtgclHiLZFLugK8qqeXFPojraqksycwbeJPEWQ/viewform"
         target="_blank"
-        rel="noopener noreferrer"
-        name='Feedback'
-      />
+        rel="noopener noreferrer">
+          <Icon name='sticky note' />
+          Feedback
+      </Menu.Item>
+      
       {walletAddress ? (
         <Menu.Menu position='right'>
           <Menu.Item>
-            <p>{walletAddress.substring(0,8)}...{walletAddress.substring(34,42)}</p>
+            <p>{walletAddress.substring(0,6)}...{walletAddress.substring(36,42)}</p>
           </Menu.Item>
           <Menu.Item>
             <Button color="red" onClick={logout}>Disconnect</Button>
